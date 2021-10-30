c6c92bdad29efcbd21306038e60e692ae667d7f6
ymcastellar <yoycas@hotmail.com>
2021-10-30 14:53:05 -0500

    files for task2

---
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -0,0 +1,59 @@
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+export class Director implements DirectorInterface {
+  workFromHome(): string {
+    return 'Working from home';
+  }
+  getCoffeeBreak(): string {
+    return 'Getting a coffee break';
+  }
+  workDirectorTasks(): string {
+    return 'Getting to director tasks';
+  }
+}
+
+export class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return 'Cannot work from home';
+  }
+  getCoffeeBreak(): string {
+    return 'Cannot have a break';
+  }
+  workTeacherTasks(): string {
+    return 'Getting to work';
+  }
+}
+
+export function createEmployee(salary: number | string): Director | Teacher {
+  if (typeof(salary) === 'number' && salary < 500) return new Teacher();
+  return new Director();
+}
+
+export function isDirector(employee: DirectorInterface | TeacherInterface): employee is Director {
+  return 'workDirectorTasks' in employee;
+}
+
+export function executeWork(employee: DirectorInterface | TeacherInterface): string {
+  if (isDirector(employee)) {
+    console.log(employee.workDirectorTasks());
+    return employee.workDirectorTasks();
+  }
+  console.log(employee.workTeacherTasks());
+  return employee.workTeacherTasks();
+}
+
+type Subjects = 'Math' | 'History';
+
+export function teachClass(todayClass:Subjects): string {
+  if (todayClass === 'Math') return 'Teaching Math';
+  if (todayClass === 'History') return 'Teaching History';
+}

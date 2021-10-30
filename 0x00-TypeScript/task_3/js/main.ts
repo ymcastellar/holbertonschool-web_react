7063f2b3ccd1c1fd733213a2605c8ae048d09564
ymcastellar <yoycas@hotmail.com>
2021-10-30 15:02:36 -0500

    adding files for task 3

---
+++ 0x00-TypeScript/task_3/js/main.ts
@@ -0,0 +1,18 @@
+/// <reference path ='./crud.d.ts' />
+import { RowID, RowElement } from './interface';
+import * as CRUD from './crud';
+
+const row: RowElement = {
+  firstName: 'Guilaume',
+  lastName: 'Salva',
+};
+
+const newRowID: RowID = CRUD.insertRow(row);
+const updatedRow: RowElement = { 
+  firstName: 'Guilaume',
+  lastName: 'Salva',
+  age: 23
+};
+
+CRUD.updateRow(newRowID, updatedRow);
+CRUD.deleteRow(newRowID);
